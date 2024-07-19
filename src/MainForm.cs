fa339808c7d232cf54d37f4b3806dad6e0f0fb1f
Blake Davis <me@blakejay.com>
2024-07-19 09:45:14 -0700

    resize

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Media;
@@ -698,6 +699,7 @@ namespace Cyotek.DitheringTest
         transform = this.GetPixelTransform();
         ditherer = new RandomDithering(); // set to random dither
         image = _image.Copy();
+        image = ResizeImage(image, 256, 256);
 
         var workerData = new WorkerData
         {
@@ -718,6 +720,7 @@ namespace Cyotek.DitheringTest
         if (usingColorImg)
         {
           colorImg = new Bitmap(Image.FromFile(colorFileName));
+          colorImg = ResizeImage(colorImg, 256, 256);
         }
 
         using (var collection = new MagickImageCollection())
@@ -812,6 +815,31 @@ namespace Cyotek.DitheringTest
       statusToolStripStatusLabel.Text = string.Empty;
       Cursor.Current = Cursors.Default;
       this.UseWaitCursor = false;
+    }
+
+    public static Bitmap ResizeImage(Image image, int width, int height)
+    {
+      var destRect = new Rectangle(0, 0, width, height);
+      var destImage = new Bitmap(width, height);
+
+      destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+      using (var graphics = Graphics.FromImage(destImage))
+      {
+        graphics.CompositingMode = CompositingMode.SourceCopy;
+        graphics.CompositingQuality = CompositingQuality.HighQuality;
+        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        graphics.SmoothingMode = SmoothingMode.HighQuality;
+        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+        using (var wrapMode = new ImageAttributes())
+        {
+          wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+          graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+        }
+      }
+
+      return destImage;
     }
   }
 }
